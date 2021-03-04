059cf8369f37048d34bee7810536a14b6d53fc7c
alecastrovisk <alexandre.acrsf@gmail.com>
2021-03-04 20:41:52 -0300

    Adicionando Geojson do mapa de Alagoas

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-//import { WeatherShower } from '@styled-icons/typicons';
-import  { MapContainer, TileLayer}  from 'react-leaflet';
+import React  from 'react';
+import { WeatherShower } from '@styled-icons/typicons';
+import  { MapContainer, TileLayer, GeoJSON}  from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import alagoasgeo from  '../../data/alagoas.json';
 
 
-
-
-const Map: React.FC = () =>  {
+const Map = () =>  {
     return(
         <>  
             <MapContainer
+                
                 center={[-9.572873,-37.153327]} 
-                zoom={8} 
-                style={ { width: '60%', height: '60%' , position: 'absolute' }}
+                zoom={9} 
+                style={ { width: '60vw', height: '60vh' , position: 'absolute'}}
                 scrollWheelZoom={false}
                 
             >
+                <GeoJSON  data={alagoasgeo} style={{weight: '0.5'}} /> 
                 <TileLayer 
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' 
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 {/* <TileLayer url={`https://api.mapbox.com/styles/v4/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/> */}
-           </MapContainer>           
+           </MapContainer>    
+           <WeatherShower/>       
         </>
         
     );
 }
 
 export default Map; 
-
