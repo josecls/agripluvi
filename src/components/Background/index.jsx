ab56e5cf859d28a362852687f81c0510f7fc45ea
josecls <jcgf1@ifal.edu.br>
2021-03-12 19:06:41 -0300

    Adiciona componente do background

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, FirstBlock, SecondBlock } from './styles';
+
+function Background() {
+  return (
+    <Container>
+      <FirstBlock />
+      <SecondBlock />
+    </Container>  
+  )
+}
+
+export default Background;
