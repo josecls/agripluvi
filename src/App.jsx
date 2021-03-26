7ba4dc55d91bd6ba7a7e399280c05a4a0211d42f
leohas <leohenrique2207@hotmail.com>
2021-03-26 08:38:57 -0300

    Added component GlobalStyle to HomePage and RegionPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import HomePage from './pages/HomePage/HomePage'
 import RegionPage from './pages/RegionPage/RegionPage'
-import { GlobalStyle } from './globalStyles'
 
 function App() {
   return (
     <>
-      <GlobalStyle />
+      <HomePage />
     </>
   );
 }
