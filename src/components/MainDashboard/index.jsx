e59593a7fb51010257dac0753fb57229514bce32
José Carlos <38336716+josecls@users.noreply.github.com>
2021-04-04 20:21:04 -0300

    Adiciona integração com a API (#17)
    * Adiciona referência da integração com a api
    
    * Finaliza integração parcial

--- src/components/MainDashboard/index.jsx
+++ src/components/MainDashboard/index.jsx
@@ -4,49 +4,57 @@ import DashboardInfo from '../DashboardInfo'
 import ReturnButton from '../ReturnButton'
 import { Container, Title, Subtitle, TextArea } from './styles'
 
-import cultura from '../../assets/images/banana.png'
-import temperatura from '../../assets/images/temperatura.svg'
-import umidade from '../../assets/images/umidade.svg'
-import chuva from '../../assets/images/chuva.svg'
+import { assets, textNormalizer } from '../../services/helper';
 
-const MainDashboard = () => {
+const MainDashboard = (props) => {
     return (
         <Container>
             <TextArea>
-                <Title>Seus resultados</Title>
-                <Subtitle>A região selecionada foi Avenida Brasil, 744, São Paulo - São Paulo</Subtitle>
+                <Title id="results">Seus resultados</Title>
+                {props.region && props.region !== 'Selecione uma região'
+                    ? (<Subtitle>A região selecionada foi {props.region}</Subtitle>)
+                    : React.Fragment
+                }
+                {!props.region || props.region === 'Selecione uma região'
+                    ? (<Subtitle>Defina uma região para obter resultados.</Subtitle>)
+                    : React.Fragment
+                }
             </TextArea>
             <Grid container justify="center" spacing={4}>
                 <Grid item>
                     <DashboardInfo
                         backgroundColor="#941C2F"
                         title="Cultura"
-                        value="Banana"
-                        img={cultura}
+                        value={props.dashboardData.cultura}
+                        img={assets[textNormalizer(props.dashboardData.cultura)] || assets['Não definido']}
+                        alt={`Ícone vetorizado de uma ${props.dashboardData.cultura}`}
                     />
                 </Grid>
                 <Grid item>
                     <DashboardInfo
                         backgroundColor="#FFD166"
                         title="Temperatura"
-                        value="0"
-                        img={temperatura}
+                        value={props.dashboardData.temperatura + " °C"}
+                        img={assets.temperatura}
+                        alt={`Ícone vetorizado de um termômetro`}
                     />
                 </Grid>
                 <Grid item>
                     <DashboardInfo
                         backgroundColor="#06D6A0"
                         title="Umidade"
-                        value="0"
-                        img={umidade}
+                        value={props.dashboardData.umidade + " g/Kg"}
+                        img={assets.umidade}
+                        alt="Ícone vetorizado de medidor de umidade"
                     />
                 </Grid>
                 <Grid item>
                     <DashboardInfo
                         backgroundColor="#B49FCC"
                         title="Chuva"
-                        value="0"
-                        img={chuva}
+                        value={props.dashboardData.chuva + " mm"}
+                        img={assets.chuva}
+                        alt="Ícone vetorizado de nuvem de chuva"
                     />
                 </Grid>
             </Grid>
---
+++ src/services/helper.js
@@ -0,0 +1,32 @@
+import cultura from '../assets/images/cultura.png';
+import mamao from '../assets/images/mamao.png';
+import banana from '../assets/images/banana.png';
+import goiaba from '../assets/images/goiaba.png';
+import abacaxi from '../assets/images/abacaxi.png';
+import acerola from '../assets/images/acerola.png';
+import temperatura from '../assets/images/temperatura.svg';
+import umidade from '../assets/images/umidade.svg';
+import chuva from '../assets/images/chuva.svg';
+
+export const assets = {
+    'Não definido': cultura,
+    mamao,
+    banana,
+    goiaba,
+    abacaxi,
+    acerola,
+    temperatura,
+    umidade,
+    chuva
+}
+
+export const textNormalizer = (text) => {
+    let formattedText = text.toLowerCase();
+
+    formattedText = formattedText.replace(/[ÀÁÂÃÄÅ]/g, 'A')
+    formattedText = formattedText.replace(/[àáâãäå]/g, 'a')
+    formattedText = formattedText.replace(/[ÈÉÊË]/g, 'E')
+    formattedText = formattedText.replace(/[^a-z0-9]/gi, '')
+
+    return formattedText.toLowerCase();
+}
